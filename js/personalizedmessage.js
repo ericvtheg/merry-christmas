e497a8f160506130564ec23f68c1c0f92ccd52d9
Eric Ventor <eventor@Erics-MacBook-Air.local>
2020-12-24 14:21:04 -0800

    added personal customizations

---
+++ js/personalizedmessage.js
@@ -0,0 +1,29 @@
+const urlParams = new URLSearchParams(window.location.search);
+const to = urlParams.get('to', '').toLowerCase();
+
+names = {
+    'theboys': ' boyssss',
+    'mormor': ' Mormor',
+    'mama': ' Mama',
+    'papa': ' Papa',
+    'ana': ' Ana',
+    'hannah': ' Hannah',
+    'karen': ' Karen'
+};
+message = {
+    'theboys': "Hope ya'll have a great day and an even better new years. More life to my cucks",
+    'mama': "As always, thank you for everything you do for me. You really are the best mom in the world and I do think about it often. You made me the person I am today. Hope you have an amazing day and rest of the year because you deserve it. Looking forward to when we get to hang out normally again!",
+    'papa': "Dawg you're the best dad in the whole wide world. You told me since I was young that I would realize even more so as I got older, and you were right. I appreciate everything you did for me to make me the man I am today. Love you.",
+    'mormor': "You are the best mormor in the world. I appreciate you so much. Soon enough we will get to see each other again! I am looking forward to it very much. I hope you are doing good and staying safe. I miss and love you so much.",
+    'ana': "Have a good one bish",
+    'hannah': "Have a fucking good ass day bish. Hope you and the fam are doing great and staying safe. I STAN THE NASSERIS",
+    'karen': "Thank you for becoming a second mom to me. I really can't express enough how grateful I am to be here and how much I appreciate all the extra you do for me and the boys. You're the best."
+};
+
+to_name = names[to];
+to_message = message[to];
+
+$(document).ready(function() {
+    $('#toReplaceTitle').text(to_name);
+    $('#toReplaceMessage > h2').text(to_message);
+});
